3b735d8a624ce5c7d8f68c9ca766b7e93cecbc16
FlowerF <1647231392@qq.com>
2023-08-27 15:01:25 +0800

    feat: add zod for formatting langchain output

--- src/utils/ai.ts
+++ src/utils/ai.ts
@@ -1,4 +1,19 @@
 import { OpenAI } from "langchain/llms/openai";
+import { StructuredOutputParser } from "langchain/output_parsers";
+import z from "zod";
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z.string().describe("the mood of the person who wrote the journal entry."),
+    summary: z.string().describe("quick summary of the entire entry."),
+    color: z
+      .string()
+      .describe(
+        "a hexadecimal color code the represents the mood of the entry. Example #0101fe for blue representing happiness.",
+      ),
+    negative: z.boolean().describe("is the journal entry negative? (i.e. does it contain negative emotions?)"),
+  }),
+);
 
 export const analyze = async (prompt: string) => {
   const model = new OpenAI({
